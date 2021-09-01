bd8dc4c2efa1633e7e96b8eda33e9b8a15032a62
Finn <finndrohberg@gmail.com>
2021-09-01 10:05:00 +0200

    Test2

--- hanuta.py
+++ hanuta.py
@@ -1,7 +1,12 @@
-true = input("Willst du ein Hanuta? [j/n]")
+def my_riegel(riegel):
+    true = input("Willst du ein " + riegel + " ? [j/n]")
 
-if true == "j":
-    print("Hanuta")
+    if true == "j":
+        print(riegel)
 
-else:
-    print("Fuck Off")
+    else:
+        print("Fuck Off")
+
+
+my_riegel(input("Was willste?"))
+
